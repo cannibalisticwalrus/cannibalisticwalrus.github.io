f361268e9748f18f129cd7fa043f8dfb89937ee1
Stephen <ghub@stephenjackiw.33mail.com>
2023-01-28 15:56:51 -0500

    A structure for a centralized menu option

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <header>Stephen Jackiw Resume(s)</header>
-        <p>At the moment, almost everything here is a default react page.  I will be updating it so that is not the case</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Stephen Jackiw's Resumes</div>
+        <div className="main-page">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="tabs-container">
+          <div className="tab">Cybersecurity</div>
+          <div className="tab">Software Development</div>
+          <div className="tab">Art Portfolio</div>
+          <div className="tab">About</div>
+          <div className="tab">Blog</div>
+        </div>
       </header>
     </div>
   );
