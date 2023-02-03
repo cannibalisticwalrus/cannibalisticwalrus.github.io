b7657f52804c97cd5d8433778f45a9362cf84366
Stephen <ghub@stephenjackiw.33mail.com>
2023-02-03 18:43:53 -0500

    Some REALLY basic css

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -5,26 +5,28 @@ import "./Layout.css";
 const Layout = () => {
   return (
     <>
-      <nav>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Resume">Resume</Link>
-        </li>
-        <li>
-          <Link to="/Portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/About">About</Link>
-        </li>
-        <li>
-          <Link to="./Blog">Blog</Link>
-        </li>
-      </nav>
+      <div className="Root">
+        <nav>
+          <li className="NavItem">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="NavItem">
+            <Link to="/Resume">Resume</Link>
+          </li>
+          <li className="NavItem">
+            <Link to="/Portfolio">Portfolio</Link>
+          </li>
+          <li className="NavItem">
+            <Link to="/About">About</Link>
+          </li>
+          <li className="NavItem">
+            <Link to="./Blog">Blog</Link>
+          </li>
+        </nav>
+      </div>
 
       <Outlet />
     </>
   );
-}
+};
 export default Layout;
