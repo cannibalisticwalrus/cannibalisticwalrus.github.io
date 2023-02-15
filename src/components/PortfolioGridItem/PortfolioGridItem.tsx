9c9fbab2eb289333426b891d991c584feca0b0c4
Stephen <ghub@stephenjackiw.33mail.com>
2023-02-15 18:38:42 -0500

    Grid Items are a fixed size and work as expected

--- src/components/PortfolioGridItem/PortfolioGridItem.tsx
+++ src/components/PortfolioGridItem/PortfolioGridItem.tsx
@@ -5,13 +5,15 @@ import TestImage from "../../Ford_logo_flat.svg.png";
 const PortfolioGridItem = () => {
   return (
     <div className="GridItem">
-      <img
-        className="GridImage"
-        src={TestImage}
-        alt="Ford Logo Found at: https://upload.wikimedia.org/wikipedia/commons/3/3e/Ford_logo_flat.svg"
-      />
-      <div className="GridItemTextContainer">
-        <div className="GridItemText">This is a test.</div>
+      <div className="GridItemContent">
+        <img
+          className="GridImage"
+          src={TestImage}
+          alt="Ford Logo Found at: https://upload.wikimedia.org/wikipedia/commons/3/3e/Ford_logo_flat.svg"
+        />
+        <div className="GridItemTextContainer">
+          <div className="GridItemText">This is a test.</div>
+        </div>
       </div>
     </div>
   );
