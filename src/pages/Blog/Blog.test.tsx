dee71485c259b02d6f8e81238dd17d714410248a
Stephen <ghub@stephenjackiw.33mail.com>
2023-03-06 22:46:07 -0500

    Blog DOM tests

--- src/pages/Blog/Blog.test.tsx
+++ src/pages/Blog/Blog.test.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./Blog";
 
-test("renders learn react link", () => {
+test("renders root component", () => {
   render(<App />);
   const rootElement = document.getElementById('#root');
   expect(rootElement).toBeInTheDocument();
+});
+
+test("renders header of Blog", () => {
+  render(<App />);
+  const titleElement = document.getElementsByClassName("BlogHeader");
+  expect(titleElement.length).toBe(1);
+  expect(titleElement[0].textContent).toBe("Blog");
+});
+
+test("renders content of Blog", () => {
+  render(<App />);
+  const titleElement = document.getElementsByClassName("BlogContent");
+  expect(titleElement.length).toBe(1);
+  expect(titleElement[0]).toBeInTheDocument();
 });
--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -3,9 +3,9 @@ import "./Blog.scss";
 const Blog = () => {
   return (
     <div className="HomeRoot" id="#root">
-      <h1>Blog</h1>
-      <p>At the moment I am testing out Blogspot for my blog provider.  Interested? Click below.</p>
-      <a style={{color: "aqua"}} href="https://stephenjackiw.blogspot.com/">Click Me!</a>
+      <h1 className="BlogHeader">Blog</h1>
+      <p className="BlogContent">At the moment I am testing out Blogspot for my blog provider.  Interested? Click below.</p>
+      <a className="BlogLink" style={{color: "aqua"}} href="https://stephenjackiw.blogspot.com/">Click Me!</a>
     </div>
   );
 };
