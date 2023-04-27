f2fed0639b3f801f968c9d710f840073e1e35813
Stephen <ghub@stephenjackiw.33mail.com>
2023-04-27 12:40:24 -0400

    Increased reactivity of Phone and Computer views.  Added icons links in Contact Me

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,5 +1,8 @@
 import "./Layout.scss";
-// import Resume from "../../resume.pdf"; 
+import { FiLinkedin } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
+import { TbBrandBlogger } from "react-icons/tb";
 
 const Layout = () => {
   return (
@@ -10,13 +13,18 @@ const Layout = () => {
           <div className="IntroductionName">Stephen Jackiw.</div>
           <div className="IntroductionOneLiner">Full-Stack Developer.</div>
           <div className="IntroductionSummary">
-            Whether creating self-documenting code, a properly normalized
-            relational database or creating a lovely minimalist user experience,
+            Whether I am creating self-documenting code, a properly normalized
+            relational database or a minimalist user experience,
             I ensure that Full-Stack development is beautiful.
           </div>
           <div className="IntroductionHireMe">Interested in hiring me?</div>
           <div className="IntroductionResumeDownload">
-            <a className="ResumeLink" href={require('../../resume.pdf')} target="_blank">
+            <a
+              className="ResumeLink"
+              href={require("../../resume.pdf")}
+              target="_blank"
+              rel="noreferrer"
+            >
               I have a resume!
             </a>
           </div>
@@ -65,22 +73,91 @@ const Layout = () => {
         </div>
       </div>
       <div className="Section">
-        <div className="ContactMe">
-          <div className="ContactMeHeader">Interested in more?</div>
-          <div className="ContactMeSocials">
-            This site is still under construction but you can see what I am
-            working on.
-            <a className="ContactMeSocialsLink" href="https://github.com/cannibalisticwalrus">My Github</a>
-            <a className="ContactMeSocialsLink" href="https://stephenjackiw.blogspot.com">My Blog</a>
+        <div className="MyExperience">
+          <div className="MyExperienceHeader">My Experience</div>
+          <div className="MyExperienceParagraph">
+            I have had the opportunity to work for a small business, a leading
+            Cybersecurity School and a Fortune 50 company providing information
+            security and developing for the web. More information is coming
+            soon! For now, download my resume.
           </div>
-          <div className="ContactMeMessages">
-            Have questions? Want to connect? Use one of these links:
-            <a className="ContactMeMessagesLink" href="https://github.com/cannibalisticwalrus">My LinkedIn</a>
-            <a className="ContactMeMessagesLink" href="mailto:website@stephenjackiw.33mail.com">My Email</a>
+          <div className="MyExperienceDownload">
+            <a
+              className="ResumeLink"
+              href={require("../../resume.pdf")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              I have a resume!
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="Section">
+        <div className="Projects">
+          <div className="ProjectsHeader">Previous Projects</div>
+          <div className="ProjectsParagraph">
+            This section is under construction :/{" "}
+          </div>
+          <div className="ProjectsParagraph">
+            Come back soon to see a few relevant projects that I am working on.
+            In the meantime you can checkout my github or my blog.
+          </div>
+          <a
+            className="ProjectsLink"
+            href="https://github.com/cannibalisticwalrus"
+          >
+            My Github
+          </a>
+          <a className="ProjectsLink" href="https://stephenjackiw.blogspot.com">
+            My Blog
+          </a>
+        </div>
+      </div>
+      <div className="Section">
+        <div className="ContactMe">
+          <div className="ContactMeHeader">Want to get a hold of me?</div>
+          <div className="ContactMeParagraph">
+            This site is still under construction but...
+          </div>
+          <div className="ContactMeParagraph">
+            If you have a question, want to get a hold of me, or checkout what I am doing feel free to use the links below.
+          </div>
+          <div className="ContactMeLinksContainer">
+  
+            <a
+              className="ContactMeLink"
+              href="https://github.com/cannibalisticwalrus"
+            >
+              <FiLinkedin />
+            </a>
+            <a
+              className="ContactMeLink"
+              href="mailto:website@stephenjackiw.33mail.com"
+            >
+              <FiMail />
+            </a>
+            <a
+              className="ContactMeLink"
+              href="https://github.com/cannibalisticwalrus"
+            >
+              <FiGithub />
+            </a>
+            <a
+              className="ContactMeLink"
+              href="https://stephenjackiw.blogspot.com"
+            >
+              <TbBrandBlogger />
+            </a>
           </div>
           <div className="ContactMeHireMe">Interested in hiring me?</div>
           <div className="ContactMeResumeDownload">
-            <a className="ResumeLink" href={require('../../resume.pdf')} target="_blank">
+            <a
+              className="ResumeLink"
+              href={require("../../resume.pdf")}
+              target="_blank"
+              rel="noreferrer"
+            >
               I have a resume!
             </a>
           </div>
